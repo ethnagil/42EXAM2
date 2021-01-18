534980c8cd8158e758d5d021ad633381d3eeaa4d
ethnagil <75136387+ethnagil@users.noreply.github.com>
2021-01-18 16:50:08 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -9,11 +9,11 @@
 
 #define BUFFER_SIZE 100
 
-int	get_next_line(char **line);
+int   get_next_line(char **line);
 void	ft_bzero(void *s, size_t n);
-char *ft_strjoin(char *dst, char *src);
-char *ft_strcpy(char *dst, char *src);
-int	ft_eol(char *buf, char *bufover);
-int	ft_strlen(char *str);
+char  *ft_strjoin(char *dst, char *src);
+char  *ft_strcpy(char *dst, char *src);
+int   ft_eol(char *buf, char *bufover);
+int   ft_strlen(char *str);
 
 #endif
