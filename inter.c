ecc0b31a6f205c62ddcd70872a33384630d36c1c
ethnagil <75136387+ethnagil@users.noreply.github.com>
2021-01-26 13:28:23 +0100

    Update inter.c

--- inter.c
+++ inter.c
@@ -1,33 +1,26 @@
-#include <unistd.h>
-/*
-int ft_elementof(char c, char *str)
-{
-	int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egillesp <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/06 10:57:29 by egillesp          #+#    #+#             */
+/*   Updated: 2021/01/06 12:52:47 by egillesp         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-*/
+#include <unistd.h>
+
 int main(int n, char **args)
 {
 	char used[255];
-	int	i;
-	int	j;
-	int	k;
+	int	i = 0;
+	int	j = 0;
 
 	if (n == 3)
 	{
-		j = 0;
-		i = 0;
-		k = 0;
-		used[0] = 0;
-		while (i <= 255)
+		while (i < 255)
 			used[i++] = 0;
 		i = 0;
 		while(args[1][i])
@@ -43,19 +36,10 @@ int main(int n, char **args)
 						write(1, &args[1][i], 1);
 					}
 				}
-//					if (!ft_elementof(args[1][i], used))
-//					{
-//						used[k] = args[1][i];
-//						k++;
-//						used[k] = 0;
-//					}
 				j++;
 			}
 			i++;
 		}
-//		i = 0;
-//		while (used[i])
-//			write(1, &used[i++], 1);
 	}
 	write(1, "\n", 1);
 	return (0);
