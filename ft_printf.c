ea9ee201b71b2c81edd38b0e6fc7ab6225f070d3
ethnagil <75136387+ethnagil@users.noreply.github.com>
2021-01-26 13:19:22 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,13 +1,3 @@
-/*
-- Vous devez gérer uniquement les conversions suivantes : s,d et x
-- Vous devez gérer la taille de champ minimal. (Nous ne testerons jamais votre programme avec une taille de champ minimal de 0)
-- Vous devez gérer uniquement le flag de précision `.`.
-
-Votre fonction devra être prototypée de la façon suivante :
-
-int ft_printf(const char *, ... );
-*/
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -15,217 +5,196 @@ int ft_printf(const char *, ... );
 /*                                                    +:+ +:+         +:+     */
 /*   By: egillesp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/14 13:21:51 by egillesp          #+#    #+#             */
-/*   Updated: 2021/01/14 15:23:19 by egillesp         ###   ########lyon.fr   */
+/*   Created: 2021/01/26 13:17:50 by egillesp          #+#    #+#             */
+/*   Updated: 2021/01/26 13:18:10 by egillesp         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
 
-int ft_write(char *str, int len)
+typedef struct s_struct
 {
-	write(1, str, len);
+	int	zeros;
+	int spaces;
+	int neg;
+	char type;
+}				t_struct;
+
+int ft_strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (0);
+	while (s[j])
+		j++;
+	return (j);
+}
+
+int		ft_write(char *pstr, int len)
+{
+	write(1, pstr, len);
 	return (len);
 }
 
-int	ft_strlen(char *str)
+char *ft_strdup(char *s)
 {
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return(i);
-}
-
-char *ft_strdup(char *str)
-{
-	int i;
+	int j = 0;
 	char *new;
 
-	if (!str)
-		return(str);
-	i = 0;
-	new = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
-	if (!new)
-		return (0);
-	while (str[i])
+	if (!s)
+		return(s);
+	j = ft_strlen(s);
+	new = (char *)malloc(sizeof(char) * (j + 1));
+	if (new)
 	{
-		new[i] = str[i];
-		i++;
-	}
-	new[i] = '\0';
-	return (new);
-}
-
-char *ft_itoa_base(unsigned long n, char *base)
-{
-	unsigned long n2;
-	int		len;
-	char	*new;
-	int		b;
-	int neg = 0;
-
-	b = ft_strlen(base);
-	n2 = n;
-	len = 0;
-	if (n == 0)    
-	{
-		new = ft_strdup("0");
-		return(new);
-	} 
-	while (n > 0)
-	{
-		n = n / b;
-		len++;
-	}
-	new = (char *)malloc(sizeof(char) * (len + 1));
-	if (!new)
-		return (0);
-	new[len] = '\0';
-	while (n2 > 0)
-	{
-		new[--len] = base[n2 % b];
-		n2 = n2 / b;
+		j = 0;
+		while(s[j])
+		{
+			new[j] = s[j];
+			j++;
+		}
+		new[j] = '\0';
 	}
 	return(new);
 }
 
-int		ft_apply_flag(char *prstr, int p1, int p2, char spec, int neg)
+char *ft_itoa_base(unsigned long n, char *base)
 {
-	int len;
-	int zero;
-	int space;
-	int count;
+	int b = 0;
+	int l = 0;
+	char *new;
+	unsigned long n2;
 
-	zero = 0;
-	space = 0;
-	count = 0;
-	if (prstr[0] == '\0')     /** ! **/
-		return (0);
-	len = ft_strlen(prstr);
-	if (p2 >= 0)
+	if (n == 0)
 	{
-		if (((spec == 's') && (len > p2)) || ((spec != 's') &&   /** ! **/
-				(len == 1) && (prstr[0] == '0') && (p2 == 0)))
-			prstr[p2] = '\0';
-		if (((spec == 'd') || (spec == 'x')) && (p2 > len))
-			zero = p2 - len;
+		new = ft_strdup("0");
+		return(new);
 	}
-	len = ft_strlen(prstr);  /***  if s, len can be truncated **/
-	if (p1 >= 0)
+	n2 = n;
+	b = ft_strlen(base);
+	while (n2 > 0)
 	{
-		if ((len + zero) < p1)
-			space = p1 - len - zero;
+		l++;
+		n2 = n2 / b;
 	}
-	if (neg)
-		space--;
-	while (space > 0)
+	new = (char *)malloc(sizeof(char) * (l + 1));
+	if (new)
 	{
-		count += ft_write(" ", 1);
-		space--;
-	}
-	if (neg)
-		count += ft_write("-", 1);
-	while (zero-- > 0)
-		count += ft_write("0", 1);
-	return (count);
-}
-
-void	ft_read_flag(char *f, int *i, int *p1, int *p2)
-{
-	int		p2neg = 0;
-
-	*p1 = -1;
-	*p2 = -1;
-	if (f[*i] == '-')
-		*i = *i + 1;
-	while((f[*i]) && (f[*i] >= '0') && (f[*i] <= '9'))
-	{
-		if (*p1 < 0)
-			*p1 = 0;
-		*p1 = (*p1 * 10) + (f[*i] - 48);
-		*i = *i + 1;
-	}
-	if (f[*i] == '.')
-	{
-		*i = *i + 1;
-		if (f[*i] == '-')
-			*i = *i + 1;
-		while((f[*i]) && (f[*i] >= '0') && (f[*i] <= '9'))
+		new[l] = '\0';
+		while (n > 0)
 		{
-			if (*p2 < 0)
-				*p2 = 0;
-			*p2 = (*p2 * 10) + (f[*i] - 48);
-			*i = *i + 1;
+			new[--l] = base[n % b];
+			n = n/b;
 		}
-		if (*p2 == -1)    /** ! **/
-			*p2 = 0;
 	}
+	return(new);
 }
 
-int		ft_print_var(char *f, va_list args, int *i)
+int		ft_apply_flags(char *pstr, t_struct *flags)
 {
-	int	p1;
-	int p2;
-	long n;
-	char *prstr;
-	char spec;
-	int	j;
-	int neg;
+	int slen = 0;
+	int len = 0;
 
-	prstr = NULL;
-	neg = 0;
-	ft_read_flag(f, i, &p1, &p2);
-	spec = f[*i];
-	if (spec == 's')
-		prstr = ft_strdup(va_arg(args, char*));
-	if (spec == 'd')
+	slen = ft_strlen(pstr);
+	if (flags->zeros >= 0)
 	{
-		n = va_arg(args, int);
-		if (n < 0)
+		if ((flags->type == 's') &&  (flags->zeros < slen))
 		{
-			neg = 1;
-			n = n * -1;
+			pstr[flags->zeros] = '\0';
+			flags->zeros = 0;
 		}
-		prstr = ft_itoa_base((unsigned long)n, "0123456789");
+		else if ((flags->type != 's') && (slen == 1) && (pstr[0] == '0') && (flags->zeros == 0))
+			pstr[0] = '\0';
+		else if ((flags->zeros > slen) && (flags->type != 's'))
+			flags->zeros = flags->zeros - slen;
+		else
+			flags->zeros = 0;
 	}
-	if (spec == 'x')
-		prstr = ft_itoa_base((unsigned long)(va_arg(args, unsigned int)), "0123456789abcdef");
-	if (!prstr)							 /** ! **/
-		prstr = ft_strdup("(null)");
-	j = ft_apply_flag(prstr, p1, p2, spec, neg);
-	j += ft_write(prstr, ft_strlen(prstr));	
-	free(prstr); 						 /** ! **/
-	return (j);
+	if (flags->zeros == -1)
+		flags->zeros = 0;
+	slen = ft_strlen(pstr);
+	flags->spaces = flags->spaces-flags->zeros-slen;
+	if (flags->neg == -1)
+		flags->spaces--;
+	while (flags->spaces-- > 0)
+		len += ft_write(" ", 1);
+	if (flags->neg == -1)
+		len += ft_write("-", 1);
+	while (flags->zeros-- > 0)
+		len += ft_write("0", 1);
+	return (len);
 }
 
-int		ft_printf(const char *f, ...)
+int		ft_print_var(char *f, va_list args, t_struct flags)
 {
-	va_list args;
-	int		i;
-	int		len;
+	char 		*pstr = NULL;
+	long		n;
+	int			len = 0;
+
+		if (flags.type == 's')
+			pstr = ft_strdup(va_arg(args, char*));
+		if (flags.type == 'd')
+		{
+			n = va_arg(args, int);
+			if (n < 0)
+				flags.neg = -1;
+			pstr = ft_itoa_base((unsigned long)(n * flags.neg), "0123456789");
+		}
+		if (flags.type == 'x')
+			pstr = ft_itoa_base((unsigned long)va_arg(args, unsigned int), "0123456789abcdef");
+		if (!pstr)
+			pstr = ft_strdup("(null)");
+		len += ft_apply_flags(pstr, &flags);
+		len += ft_write(pstr, ft_strlen(pstr));
+		free (pstr);
+		return (len);
+}
+
+int ft_printf(const char *f, ... )
+{
+	va_list	args;
+	t_struct flags;
+	int		i = 0;
+	int		len = 0;
 
 	va_start(args, f);
-	i = 0;
-	len = 0;
 	while (f[i])
 	{
 		if (f[i] == '%')
 		{
+			flags.zeros = -1; flags.spaces = 0; flags.neg = 1; flags.type = ' ';
 			i++;
-			len += ft_print_var((char *)f, args, &i);
+			while ((f[i] >= '0') && (f[i] <= '9'))
+				flags.spaces = (flags.spaces * 10) + (f[i++] - 48);
+			if (f[i] == '.')
+			{
+				i++;
+				flags.zeros = 0;
+				while ((f[i] >= '0') && (f[i] <= '9'))
+					flags.zeros = (flags.zeros * 10) + (f[i++] - 48);
+			}
+			flags.type = f[i];
+			len += ft_print_var((char *)f, args, flags);
 		}
 		else
 		{
 			write(1, &f[i], 1);
 			len++;
 		}
-		if (f[i])      /** ! **/
+		if (f[i])
 			i++;
 	}
-	va_end(args);       /** ! **/
-	return(len);
+	return (len);
 }
+
+/*
+- Vous devez gérer uniquement les conversions suivantes : s,d et x
+- Vous devez gérer la taille de champ minimal. (Nous ne testerons jamais votre programme avec une taille de champ minimal de 0)
+- Vous devez gérer uniquement le flag de précision `.`.
+
+Votre fonction devra être prototypée de la façon suivante :
+
+int ft_printf(const char *, ... ); */
