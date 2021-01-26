68806c3a2a9dfa4b4612a4d0a8ebe80a321915c3
ethnagil <75136387+ethnagil@users.noreply.github.com>
2021-01-26 13:23:44 +0100

    Update main_args.c

--- main_args.c
+++ main_args.c
@@ -1,41 +1,33 @@
 // ./a.out d $'|%10.15x|\n' 55
 
-
 #include <stdio.h>
 #include <limits.h>
-#include <strings.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int	ft_printf(char *f, ...);
 
 int main(int n, char **s)
 {
-	(void)n;
 	if (s[1][0] == 's')
 	{
-	printf(s[2], s[3]);
-	ft_printf(s[2], s[3]);
+		printf(s[2], s[3]);
+		ft_printf(s[2], s[3]);
+		printf(s[2], NULL);
+		ft_printf(s[2], NULL);
+		printf(s[2], "0");
+		ft_printf(s[2], "0");
 	}
-	else
+	if (s[1][0] != 's')
 	{
-	printf(s[2], atoi(s[3]));
-	ft_printf(s[2], atoi(s[3]));
+		printf(s[2], atoi(s[3]));
+		ft_printf(s[2], atoi(s[3]));
+		printf(s[2], NULL);
+		ft_printf(s[2], NULL);
+			printf(s[2], INT_MIN);
+		ft_printf(s[2], INT_MIN);
+			printf(s[2], INT_MAX);
+		ft_printf(s[2], INT_MAX);
 	}
-
-	if (s[1][0] == 's')
-	{
-	printf(s[2], NULL);
-	ft_printf(s[2], NULL);
-	}
-	else
-	{
-	printf(s[2], INT_MAX);
-	ft_printf(s[2], INT_MAX);
-	printf(s[2], INT_MIN);
-	ft_printf(s[2], INT_MIN);
-	}
-
-	while (2) ;
 	return (0);
-	
 }
